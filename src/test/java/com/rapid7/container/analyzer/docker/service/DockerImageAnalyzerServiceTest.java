022d242e3ad1b04e69535f34d8d34b3d2acd58d9
Alex Baluha <abaluha@hubspot.com>
2020-09-16 12:10:57 -0400

    Fixed rpm dbpath command
    Closes: https://github.com/rapid7/docker-image-analyzer/issues/4

--- src/test/java/com/rapid7/container/analyzer/docker/service/DockerImageAnalyzerServiceTest.java
+++ src/test/java/com/rapid7/container/analyzer/docker/service/DockerImageAnalyzerServiceTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DockerImageAnalyzerServiceTest {
 
   @Test
-  public void test() throws IOException {
+  public void testAlpine() throws IOException {
     // Given
     File tarFile = new File(getClass().getClassLoader().getResource("containers/fakealpine.tar").getFile());
     ImageId expectedId = new ImageId("sha256:7be494284b1dea6cb2012a5ef99676b4ec22868d9ee235c60e48181542d70fd5");
@@ -23,6 +23,29 @@ class DockerImageAnalyzerServiceTest {
     long expectedSize = 119296;
     long expectedLayers = 2;
     long expectedPackages = 66;
+
+    // When
+    DockerImageAnalyzerService analyzer = new DockerImageAnalyzerService(null);
+    Path tmpdir = Files.createTempDirectory("r7dia");
+    Image image = analyzer.analyze(tarFile, tmpdir.toString());
+
+    // Then
+    assertEquals(expectedId, image.getId());
+    assertEquals(expectedOs, image.getOperatingSystem());
+    assertEquals(expectedSize, image.getSize());
+    assertEquals(expectedLayers, image.getLayers().size());
+    assertEquals(expectedPackages, image.getPackages().size());
+  }
+
+  @Test
+  public void testCentos() throws IOException {
+    // Given
+    File tarFile = new File(getClass().getClassLoader().getResource("containers/test_image_centos.tar").getFile());
+    ImageId expectedId = new ImageId("sha256:48650444e4191276a912ac90816a4ce1e86f76419e004d445a2e42809fec05f7");
+    OperatingSystem expectedOs = new OperatingSystem("Red Hat", "Linux", "Linux", "x86_64", null, "Red Hat Linux");
+    long expectedSize = 201543168;
+    long expectedLayers = 3;
+    long expectedPackages = 138;
 
     // When
     DockerImageAnalyzerService analyzer = new DockerImageAnalyzerService(null);
