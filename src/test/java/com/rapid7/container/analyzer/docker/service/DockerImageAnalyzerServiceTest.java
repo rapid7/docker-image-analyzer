b55ad0f58a72dd82c2f37735ee40db4406ed8ee8
Brandon Holden <brandon_holden@rapid7.com>
2021-08-03 16:04:18 -0700

    Fixing test imports

--- src/test/java/com/rapid7/container/analyzer/docker/service/DockerImageAnalyzerServiceTest.java
+++ src/test/java/com/rapid7/container/analyzer/docker/service/DockerImageAnalyzerServiceTest.java
@@ -1,8 +1,8 @@
 package com.rapid7.container.analyzer.docker.service;
 
-import com.rapid7.docker.model.image.Image;
-import com.rapid7.docker.model.image.ImageId;
-import com.rapid7.docker.model.image.OperatingSystem;
+import com.rapid7.container.analyzer.docker.model.image.Image;
+import com.rapid7.container.analyzer.docker.model.image.ImageId;
+import com.rapid7.container.analyzer.docker.model.image.OperatingSystem;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -39,29 +39,6 @@ class DockerImageAnalyzerServiceTest {
   public void testCentos() throws IOException {
     // Given
     File tarFile = new File(getClass().getClassLoader().getResource("containers/centos/test_image_centos.tar").getFile());
-    ImageId expectedId = new ImageId("sha256:bcbdfe9aa33a028bedc671ecfe56ce628ff64a8ad1b2fa46855c03955fcdc5bf");
-    long expectedSize = 72704;
-    // omit OS check because this image tar was built from scratch
-    long expectedLayers = 3;
-    long expectedPackages = 1;
-
-    // When
-    DockerImageAnalyzerService analyzer = new DockerImageAnalyzerService(null);
-    Path tmpdir = Files.createTempDirectory("r7dia");
-    Image image = analyzer.analyze(tarFile, tmpdir.toString());
-
-    // Then
-    assertEquals(expectedId, image.getId());
-    assertEquals(expectedSize, image.getSize());
-    assertEquals(expectedLayers, image.getLayers().size());
-    assertEquals(expectedPackages, image.getPackages().size());
-    assertEquals("A set of system configuration and setup files", image.getPackages().stream().findFirst().get().getDescription());
-  }
-
-  @Test
-  public void testWhatever() throws IOException {
-    // Given
-    File tarFile = new File(getClass().getClassLoader().getResource("containers/48ac30562192.tar").getFile());
     ImageId expectedId = new ImageId("sha256:bcbdfe9aa33a028bedc671ecfe56ce628ff64a8ad1b2fa46855c03955fcdc5bf");
     long expectedSize = 72704;
     // omit OS check because this image tar was built from scratch
