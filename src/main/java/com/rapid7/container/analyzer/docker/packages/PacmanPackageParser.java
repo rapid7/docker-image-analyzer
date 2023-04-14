4bfc416dc7113978a72377ae7fa4b673e4e66abb
Patrick <patrick_hayes@rapid7.com>
2023-04-14 11:14:29 -0700

    SI-19047

--- src/main/java/com/rapid7/container/analyzer/docker/packages/PacmanPackageParser.java
+++ src/main/java/com/rapid7/container/analyzer/docker/packages/PacmanPackageParser.java
@@ -85,7 +85,7 @@ public class PacmanPackageParser implements PackageParser<InputStream> {
         try {
           packages.add(new Package(source, PackageType.PACMAN, operatingSystem, pkg, version, description, installedSize, maintainer, homepage, license));
         } catch (PackageValidationException pve) {
-          LOGGER.warn(pve.getMessage());
+          LOGGER.info(pve.getMessage());
         }
       }
     }
